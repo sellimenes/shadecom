6575957322f516a08f4beafce654de1b1ecdb5d9
Selim Enes <sellim81@gmail.com>
2024-01-01 12:37:15 +0300

    add scrollarea to layout

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -2,13 +2,16 @@ import { AdminSidebar } from '@/components/(Admin)/AdminSidebar/AdminSidebar';
 import { getSettings } from '@/lib/actionsSettings';
 
 import classes from './AdminLayout.module.css';
+import { ScrollArea } from '@mantine/core';
 
 const layout = async ({ children }: { children: any }) => {
   const settingsData = await getSettings();
   return (
     <div className={classes.adminWrapper}>
       <AdminSidebar settingsData={settingsData} />
-      <div className={classes.childrenWrapper}>{children}</div>
+      <ScrollArea w="100%">
+        <div className={classes.childrenWrapper}>{children}</div>
+      </ScrollArea>
     </div>
   );
 };
