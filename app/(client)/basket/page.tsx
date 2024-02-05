e60307a15695de16c8206916dee74eb56d4cb1f4
Selim Enes <sellim81@gmail.com>
2024-02-05 20:57:59 +0300

    basket page no items

--- app/(client)/basket/page.tsx
+++ app/(client)/basket/page.tsx
@@ -1,16 +1,27 @@
 import {
+  Box,
     Container,
     Text,
+    Flex,
+    Button,
   } from '@mantine/core';
+  import classes from './BasketPage.module.css';
+import { IconBasketSearch } from '@tabler/icons-react';
 
 type Props = {}
 
 const BasketPage = (props: Props) => {
   return (
     <Container size="1600px" w="90%">
-        <Text size="xl">
-          Basket
-        </Text>
+        <Box mt='2rem' p='1rem' className={classes.NoItemBox}>
+          <Flex gap='1rem' align='center'>
+            <Box p='md' bg='primary' className={classes.NoItemIcon}>
+              <IconBasketSearch size={36} color='white' />
+            </Box>
+            <Text size='xl'>No items in your basket.</Text>
+          </Flex>
+          <Button component='a' href='/' variant='filled' color='primary' size='lg' radius='md'>Continue Shopping</Button>
+        </Box>
     </Container>
   )
 }
